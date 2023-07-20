a3970e0569a4ce64c2ba77afbfcdd19d354ecaa5
TheJumbov12 <114319070+TheJumbov12@users.noreply.github.com>
2023-07-20 16:36:37 -0500

    Add files via upload

---
+++ ammobox10.lua
@@ -0,0 +1,46 @@
+if SERVER then
+    AddCSLuaFile()
+end
+
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = bigammoname
+ENT.Author = "The Jumbo"
+ENT.Spawnable = true
+ENT.Category = "Jumbo Ammo"
+
+
+
+function ENT:Initialize()
+    if SERVER then
+        self:SetModel(bigammomodel) 
+        self:SetSolid(SOLID_VPHYSICS)
+        self:SetUseType(SIMPLE_USE)
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetCollisionGroup(1)
+
+        self.usesLeft = bigammocount
+
+    end
+end
+
+
+function ENT:Use(activator, caller)
+    if not IsValid(caller) or not caller:IsPlayer() then return end
+
+    local weapon = caller:GetActiveWeapon()
+    if not IsValid(weapon) then return end
+
+    local ammoType = weapon:GetPrimaryAmmoType()
+    local ammoCount = weapon:GetMaxClip1()
+
+    if self.usesLeft > 0 and ammoType ~= -1 and ammoCount > 0 then
+        caller:GiveAmmo(ammoCount, ammoType)
+        self.usesLeft = self.usesLeft - 1
+
+        if self.usesLeft == 0 then
+            self:Remove()
+        end
+    end
+end
---
+++ config.lua
@@ -0,0 +1,15 @@
+-- This is proboly the most un-optimized way of doing this but, whatever.
+-- All of this is required!
+
+smallammomodel = "models/Items/BoxSRounds.mdl"
+medammomodel = "models/Items/BoxMRounds.mdl"
+bigammomodel = "models/Items/ammocrate_ar2.mdl"
+
+smallammocount = 2
+medammocount = 5
+bigammocount = 10
+
+smallammoname = "Ammo Box (2 Uses)"
+medammoname = "Ammo Box (5 Uses)"
+bigammoname = "Ammo Box (10 Uses)"
+
